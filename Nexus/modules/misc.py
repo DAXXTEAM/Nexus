521664edb1a1b47a775b667f3ea3bc4e0ad82f7e
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-11 18:18:36 +0530

    Create misc.py

---
+++ Nexus/modules/misc.py
@@ -0,0 +1,17 @@
+from asyncio
+from Nexus import Nexus
+from pyrogram import filters, Client, enums
+from pyrogram.enums import ParseMode
+from pyrogram.types import *
+
+@Nexus.on_message(filters.command("id"))
+def ids(_, message):
+    reply = message.reply_to_message
+    if reply:
+        message.reply_text(
+            f"ʏᴏᴜʀ ɪᴅ: {message.from_user.id}\n{reply.from_user.first_name}'s ɪᴅ: {reply.from_user.id}\nᴄʜᴀᴛ ɪᴅ: {message.chat.id}"
+        )
+    else:
+        message.reply(
+            f"ʏᴏᴜʀ ɪᴅ: {message.from_user.id}\nᴄʜᴀᴛ ɪᴅ: {message.chat.id}"
+        )
