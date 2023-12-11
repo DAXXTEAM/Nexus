86965819a819520179c2d693372ca0d9db45b706
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-11 16:54:48 +0530

    Add files via upload

---
+++ Nexus/__init__.py
@@ -0,0 +1,60 @@
+import asyncio
+import logging
+import time
+from pytgcalls import PyTgCalls
+from importlib import import_module
+from dotenv import load_dotenv
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+
+
+
+loop = asyncio.get_event_loop()
+load_dotenv()
+boot = time.time()
+
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.INFO,
+)
+
+
+
+Nexus = Client(
+    ":Nexus:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+userbot = Client(
+    ":userbot:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION_STRING,
+)
+
+pytgcalls = PyTgCalls(userbot)
+
+
+
+
+
+async def Nexus_bot():
+    global BOT_ID, BOT_NAME, BOT_USERNAME
+    await Nexus.start()
+    await userbot.start()
+    await pytgcalls.start()
+    getme = await Nexus.get_me()
+    BOT_ID = getme.id
+    BOT_USERNAME = getme.username
+    if getme.last_name:
+        BOT_NAME = getme.first_name + " " + getme.last_name
+    else:
+        BOT_NAME = getme.first_name
+
+
+loop.run_until_complete(Nexus_bot())
+
+
