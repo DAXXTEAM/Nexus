1a0b7ae16b8ac91249ce5d2138b11734df735cf9
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-11 22:48:03 +0530

    Update __main__.py

--- Nexus/__main__.py
+++ Nexus/__main__.py
@@ -12,7 +12,7 @@ loop = asyncio.get_event_loop()
 async def sumit_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("Nexus.modules." + all_module)
-    print("»»»»  ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
+    print(""𝐘𝐨𝐮𝐫 𝐛𝐨𝐭 𝐡𝐚𝐬 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐬𝐭𝐚𝐫𝐭𝐞𝐝 𝐚𝐧𝐝 𝐢𝐬 𝐫𝐞𝐚𝐝𝐲 𝐭𝐨 𝐚𝐬𝐬𝐢𝐬𝐭! 🚀"")
     await idle()
     print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
 
