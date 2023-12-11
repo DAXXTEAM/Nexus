f7b4a4d5b3514bbc4b0340e9f3518b58a4da0a49
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-12 01:10:26 +0530

    Update misc.py

--- Nexus/modules/misc.py
+++ Nexus/modules/misc.py
@@ -1,7 +1,5 @@
 from Nexus import Nexus
 import aiohttp
-from pyrogram.types import InputMediaPhoto
-from telegraph import upload_file
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from pyrogram.types import *
@@ -75,72 +73,3 @@ async def getid(client, message):
 
 # --------------------------------------------------------------------------------- #
 # --------------------------------------------------------------------------------- #
-
-@Nexus.on_message(filters.command(["github", "git"]))
-async def github(_, message):
-    if len(message.command) != 2:
-        await message.reply_text("/git DAXXTEAM")
-        return
-
-    username = message.text.split(None, 1)[1]
-    URL = f'https://api.github.com/users/{username}'
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(URL) as request:
-            if request.status == 404:
-                return await message.reply_text("404")
-
-            result = await request.json()
-
-            try:
-                url = result['html_url']
-                name = result['name']
-                company = result['company']
-                bio = result['bio']
-                created_at = result['created_at']
-                avatar_url = result['avatar_url']
-                blog = result['blog']
-                location = result['location']
-                repositories = result['public_repos']
-                followers = result['followers']
-                following = result['following']
-
-                caption = f"""ɢɪᴛʜᴜʙ ɪɴғᴏ ᴏғ {name}
-                
-ᴜsᴇʀɴᴀᴍᴇ: {username}
-ʙɪᴏ: {bio}
-ʟɪɴᴋ: [Here]({url})
-ᴄᴏᴍᴩᴀɴʏ: {company}
-ᴄʀᴇᴀᴛᴇᴅ ᴏɴ: {created_at}
-ʀᴇᴩᴏsɪᴛᴏʀɪᴇs: {repositories}
-ʙʟᴏɢ: {blog}
-ʟᴏᴄᴀᴛɪᴏɴ: {location}
-ғᴏʟʟᴏᴡᴇʀs: {followers}
-ғᴏʟʟᴏᴡɪɴɢ: {following}"""
-
-            except Exception as e:
-                print(str(e))
-                pass
-
-    # Create an inline keyboard with a close button
-    close_button = InlineKeyboardButton("Close", callback_data="close")
-    inline_keyboard = InlineKeyboardMarkup([[close_button]])
-
-    # Send the message with the inline keyboard
-    await message.reply_photo(photo=avatar_url, caption=caption, reply_markup=inline_keyboard)
-
-# --------------------------------------------------------------------------------- #
-# --------------------------------------------------------------------------------- #
-
-@Nexus.on_message(filters.command(["tgm" , "link"]))
-def ul(_, message):
-    reply = message.reply_to_message
-    if reply.media:
-        i = message.reply("ᴘʀᴏᴄᴇssɪɴɢ")
-        path = reply.download()
-        fk = upload_file(path)
-        for x in fk:
-            url = "https://telegra.ph" + x
-
-        i.edit(f' ᴄᴏᴘʏ ᴛʜɪs ʟɪɴᴋ {url}')
-
