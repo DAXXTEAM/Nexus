0eb63d712472d7b602aa1aaf30a2e2c852ac02d8
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-16 10:53:44 +0530

    Update chatsdb.py

--- Nexus/Helper/db/chatsdb.py
+++ Nexus/Helper/db/chatsdb.py
@@ -2,38 +2,34 @@ from config import MONGO_URL
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 mongo = MongoCli(MONGO_URL)
-db = mongo.users
+db = mongo.chats
 
-db = db.users
-
-
+db = db.chatsdb
 
 
-async def get_users():
-  user_list = []
-  async for user in db.users.find({"user": {"$gt": 0}}):
-    user_list.append(user['user'])
-  return user_list
+async def get_chats():
+  chat_list = []
+  async for chat in db.chats.find({"chat": {"$lt": 0}}):
+    chat_list.append(chat['chat'])
+  return chat_list
 
-
-async def get_user(user):
-  users = await get_users()
-  if user in users:
+async def get_chat(chat):
+  chats = await get_chats()
+  if chat in chats:
     return True
   else:
     return False
 
-async def add_user(user):
-  users = await get_users()
-  if user in users:
+async def add_chat(chat):
+  chats = await get_chats()
+  if chat in chats:
     return
   else:
-    await db.users.insert_one({"user": user})
+    await db.chats.insert_one({"chat": chat})
 
-
-async def del_user(user):
-  users = await get_users()
-  if not user in users:
+async def del_chat(chat):
+  chats = await get_chats()
+  if not chat in chats:
     return
   else:
-    await db.users.delete_one({"user": user})
+    await db.chats.delete_one({"chat": chat})
