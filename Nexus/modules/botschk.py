7278ed5f318fcb48bda79cb39b8b99b441998f3e
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-17 13:07:22 +0530

    Create botschk.py

---
+++ Nexus/modules/botschk.py
@@ -0,0 +1,30 @@
+import asyncio
+import random
+from pyrogram import filters
+from Nexus import Nexus, userbot
+from config import SUDO_USERS
+
+BOT_LIST = ["YumikooBot", "NexikoBot"]
+
+
+
+@Nexus.on_message(filters.command("botschk") & filters.user(SUDO_USERS))
+async def bots_chk(Nexus, message):
+    msg = await message.reply_photo(photo="https://telegra.ph/file/4d303296e4fac9a40ea07.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
+    response = "**ʙᴏᴛs sᴛᴀᴛᴜs ᴅᴇᴀᴅ ᴏʀ ᴀʟɪᴠᴇ ᴄʜᴇᴄᴋᴇʀ**\n\n"
+    for bot_username in BOT_LIST:
+        try:
+            bot = await userbot.get_users(bot_username)
+            bot_id = bot.id
+            await asyncio.sleep(0.5)
+            bot_info = await userbot.send_message(bot_id, "/start")
+            await asyncio.sleep(3)
+            async for bot_message in userbot.get_chat_history(bot_id, limit=1):
+                if bot_message.from_user.id == bot_id:
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
+                else:
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**\n\n"
+        except Exception:
+            response += f"╭⎋ {bot_username}\n╰⊚ **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ❌**\n"
+    
+    await msg.edit_text(response)
