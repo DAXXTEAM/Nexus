875dea24999944961056a4aee2ae1e871f2fbfaf
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-09 17:48:38 +0530

    Update __main__.py

--- Nexus/__main__.py
+++ Nexus/__main__.py
@@ -4,12 +4,18 @@ from pyrogram import idle
 from Nexus import Nexus
 from Nexus.modules import ALL_MODULES
 
-async def Nexus_bot():
+ 
+
+loop = asyncio.get_event_loop()
+
+
+async def daxx_boot():
     for all_module in ALL_MODULES:
-        importlib.import_module("Nexus.modules." + all_module)
-    print("""𝐘𝐨𝐮𝐫 𝐛𝐨𝐭 𝐡𝐚𝐬 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐬𝐭𝐚𝐫𝐭𝐞𝐝 𝐚𝐧𝐝 𝐢𝐬 𝐫𝐞𝐚𝐝𝐲 𝐭𝐨 𝐚𝐬𝐬𝐢𝐬𝐭""")
+        importlib.import_module("Zuli.modules." + all_module)
+    print("»»»» ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ. ✨ 🎉")
     await idle()
     print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
 
+
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(Nexus_bot())
+    loop.run_until_complete(daxx_boot())
