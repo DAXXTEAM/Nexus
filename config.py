2bdc229ba5ed0fa4feb560af3b97c015a3f758da
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-09 11:45:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,29 @@ from os import getenv
 
 
 API_ID = int(getenv("API_ID", "24509589"))
+
+
+
 API_HASH = getenv("API_HASH", "717cf21d94c4934bcbe1eaa1ad86ae75")
+
+
 BOT_USERNAME = getenv("BOT_USERNAME", "NexikoBot")
+
 BOT_TOKEN = getenv("BOT_TOKEN", "6732878880:AAEJUC3qc04el1oR7djxunhmMb2E_bYguo4")
+
 OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
+
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6204761408 6691393517").split()))
+
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority")
+
 SESSION_STRING = getenv("SESSION_STRING", "BQBiMZkAYSiUTc9Sd6xH0moqNBduWlG3SoIAjaJ5h1bX5rfUrYtdIXbo4iIYZ6-AG0gIFN2A0j37fUMAzqJ9RBcY_DQ6x40n6x7WECrst7mRUS57iRKTGALGbwbDJqfW0D5HG96vItpSGYjug5O82ebSDjp29HOQZWJ_iKFYw5bt_oX7u7CouSp1ndaOi0Bwym11U0m0XfCd01Rl1JQSTZq_6TXfdL5EKhQgDVcPW4RZ904RtJnzQJRKNYy_RyWTFPaOx5lAVJzWqp0uU9ajyLfnv1Cu89KjlX0R9o3Gek58b-zRbc2gt6VSxhCFWFr2T1s03W1qhgWmgqIVUHZTGMqzzuj51QAAAAF7IdPRAA")
+
 CHANNEL = getenv("CHANNEL", "https://t.me/ALLTYPECC")
+
+
 SUPPORT = getenv("SUPPORT", "https://t.me/HEROKUFREECC")
+
 OWNER_USERNAME = getenv("OWNER_USERNAME","iam_daxx")
+
 PING_PIC = ["https://telegra.ph/file/7c25ef427c9f3cded5577.jpg","https://telegra.ph/file/625d235cc0a22fb8525b5.jpg","https://telegra.ph/file/1c62254d59baf7f968ba7.jpg","https://telegra.ph/file/7a0553bd4664486ab3008.jpg","https://telegra.ph/file/7b4dfa606e6f23961d30e.jpg", "https://telegra.ph/file/2773dec98d87b8562618c.jpg", "https://telegra.ph/file/80353d02e0368b71d2666.jpg","https://telegra.ph/file/6e5331dc4bef87464ea1c.jpg","https://telegra.ph/file/199a2e44cb8e77bb21b34.jpg","https://telegra.ph/file/8371bcd8952d089f9ec05.jpg","https://telegra.ph/file/f970e559dd1bb96fced1a.jpg","https://telegra.ph/file/59a305f8ce0c4e85949cc.jpg"]
