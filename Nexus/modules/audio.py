9f19ade53018e47e9f18eaa15149286d2f202521
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-09 16:37:04 +0530

    Update audio.py

--- Nexus/modules/audio.py
+++ Nexus/modules/audio.py
@@ -17,9 +17,9 @@ from Nexus.Helper import requirements as rq
 from Nexus.Helper.errors import DurationLimitError
 from Nexus.Helper.requirements import get_audio_stream
 from pytgcalls.types import Update
-from pytgcalls.types import AudioPiped, AudioVideoPiped, AudioQuality, AudioParameters
+#from pytgcalls.types import AudioPiped, AudioVideoPiped, AudioQuality, AudioParameters
 
-
+from pytgcalls.types import *
 
 DURATION_LIMIT = 300
 
