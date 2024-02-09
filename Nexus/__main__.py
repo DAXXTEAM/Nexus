877c58bd29857ecaadfb3ea7df6a2fd6a09cfa6a
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-09 16:06:08 +0530

    Update and rename __main__ to __main__.py

--- Nexus/__main__.py
+++ Nexus/__main__.py
@@ -4,15 +4,12 @@ from pyrogram import idle
 from Nexus import Nexus
 from Nexus.modules import ALL_MODULES
 
- 
-
 async def Nexus_bot():
     for all_module in ALL_MODULES:
         importlib.import_module("Nexus.modules." + all_module)
     print("""𝐘𝐨𝐮𝐫 𝐛𝐨𝐭 𝐡𝐚𝐬 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐬𝐭𝐚𝐫𝐭𝐞𝐝 𝐚𝐧𝐝 𝐢𝐬 𝐫𝐞𝐚𝐝𝐲 𝐭𝐨 𝐚𝐬𝐬𝐢𝐬𝐭""")
     await idle()
     print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
-
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(Nexus_bot())
